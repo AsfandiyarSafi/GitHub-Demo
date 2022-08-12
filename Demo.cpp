b8d2d3274d4ab386481ee21f1f6a5ad8ee82796e
Asfandiyar Safi <safi.asfnd@gmail.com>
2022-08-12 11:30:07 +0500

    Demo added

---
+++ Demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a=2,b=3,sum;
+    sum = a+b;
+    cout << "The sum is: " << sum << endl;
+    
+    return 0;
+}
