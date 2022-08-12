7454700636d753ad0d4735f6019ce1342bdb74cd
Asfandiyar Safi <safi.asfnd@gmail.com>
2022-08-12 12:46:07 +0500

    Changes to Sub.cpp

---
+++ Sub.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a=4,b=5,sub;
+    sub = b-a;
+    cout << "The subtraction is: " << sub << endl;
+    return 0;
+}
